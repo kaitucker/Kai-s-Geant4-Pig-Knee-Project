d8ae2e412728b2292cce0e52cecd9e3e88dd2be8
kaitucker <kaituckersample@gmail.com>
2025-10-14 17:09:24 -0400

    Create ActionInitialization.hh

---
+++ ActionInitialization.hh
@@ -0,0 +1,29 @@
+#include "SteppingAction.hh"
+#include "RunAction.hh"
+#include "G4Step.hh"
+#include "G4TouchableHandle.hh"
+#include "G4LogicalVolume.hh"
+
+SteppingAction::SteppingAction(RunAction* runAction, const G4String& targetLogicalName)
+ : G4UserSteppingAction(), fRunAction(runAction), fTargetLogicalName(targetLogicalName) {}
+
+SteppingAction::~SteppingAction() {}
+
+void SteppingAction::UserSteppingAction(const G4Step* step) {
+  if (!step) return;
+  auto prePoint = step->GetPreStepPoint();
+  if (!prePoint) return;
+  auto touch = prePoint->GetTouchableHandle();
+  if (!touch) return;
+  auto volume = touch->GetVolume();
+  if (!volume) return;
+  G4LogicalVolume* lv = volume->GetLogicalVolume();
+  if (!lv) return;
+
+  if (lv->GetName() == fTargetLogicalName) {
+    G4double edep = step->GetTotalEnergyDeposit();
+    if (edep > 0.) {
+      fRunAction->AddEdep(edep);
+    }
+  }
+}
