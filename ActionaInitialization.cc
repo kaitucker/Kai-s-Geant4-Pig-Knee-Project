a66ec9fed05e7fee578b66f89456e107a87389db
kaitucker <kaituckersample@gmail.com>
2025-10-14 17:06:04 -0400

    Create ActionaInitialization.cc

---
+++ ActionaInitialization.cc
@@ -0,0 +1,24 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "SteppingAction.hh"
+ActionInitialization::ActionInitialization()
+: G4VUserActionInitialization()
+{}
+ActionInitialization::~ActionInitialization()
+{}
+void ActionInitialization::BuildForMaster() const
+{
+// Only the RunAction is needed on the master for summaries
+SetUserAction(new RunAction());
+}
+void ActionInitialization::Build() const
+{
+// create the RunAction first so pointers can be passed to other actions
+RunAction* runAction = new RunAction();
+SetUserAction(new PrimaryGeneratorAction()); // must be registered
+SetUserAction(runAction); // RunAction for this worker
+// Register SteppingAction and pass runAction pointer so it can call AddEdep(...)
+SetUserAction(new SteppingAction(runAction, "PatellarTendonLog"));
+
+}
