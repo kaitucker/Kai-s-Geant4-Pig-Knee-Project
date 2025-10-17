86be298bc66f91eced33e7d922a8401b101f26f3
kaitucker <kaituckersample@gmail.com>
2025-10-16 21:11:03 -0400

    Refactor SteppingAction constructor to use EventAction

--- SteppingAction.hh
+++ SteppingAction.hh
@@ -4,16 +4,16 @@
 #include "G4UserSteppingAction.hh"
 #include "G4String.hh"
 
-class RunAction;
 class G4Step;
 
 class SteppingAction : public G4UserSteppingAction {
 public:
-  SteppingAction(RunAction* runAction, const G4String& targetLogicalName);
+  SteppingAction (EventAction* eventAction, const G4String& targetLogicalName);
   ~SteppingAction() override;
   void UserSteppingAction(const G4Step* step) override;
+
 private:
-  RunAction* fRunAction;
+  EventAction* fEventAction;
   G4String fTargetLogicalName;
 };
 #endif
